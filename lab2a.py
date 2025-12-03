5007226897f4dbdce3b8bbb6c435b9eff756e5f2
Avraham Abel <avraham.lewis.abel@gmail.com>
2025-12-03 10:57:35 -0500

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+# OPS435 - Lab 2
+# lab2a.py
+# Author: Avraham Abel
 
 name = "John"
 age = 20
 
 # print("Hello " + name + ", you are " + str(age) + " years old")
-
 colour = input("TYpe in a colour and press enter: ")
-
 print("You just typed in: " + colour)
